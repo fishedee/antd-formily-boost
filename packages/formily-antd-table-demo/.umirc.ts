0b0c6d845c1e34a37bb627909194664974f822b1
fishedee <306766045@qq.com>
2021-08-03 15:06:34 +0800

    完成基础目录

--- packages/formily-antd-table-demo/.umirc.ts
+++ packages/formily-antd-table-demo/.umirc.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from 'umi';
+import path from 'path';
 
 export default defineConfig({
   nodeModulesTransform: {
     type: 'none',
   },
-  routes: [
-    { path: '/', component: '@/pages/index' },
-  ],
+  routes: [{ path: '/', component: '@/pages/index' }],
+  alias: {
+    'formily-antd-table': path.resolve(__dirname, '../formily-antd-table'),
+  },
   fastRefresh: {},
 });
