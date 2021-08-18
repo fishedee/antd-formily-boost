c0ed2acdb63320d1fa57fcdb29bed4083157f998
fishedee <306766045@qq.com>
2021-08-18 19:07:39 +0800

    去掉Link组件对umi的依赖

--- src/Link.tsx
+++ src/Link.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
 import { observer, useField } from '@formily/react';
-import { useHistory } from 'umi';
 
 type LinkProps = {
-    to:
-        | {
-              pathname?: string;
-              query?: object;
-              state?: object;
-          }
-        | string;
+    to: string;
     onClick?: () => void;
 };
 
 const MyLink: React.FC<LinkProps> = observer((props) => {
-    const history = useHistory();
     const field = useField();
     let onClick = props.onClick
         ? props.onClick
         : () => {
-              history.push(props.to);
+              window.location.href = props.to;
           };
     return <a onClick={onClick}>{field.title}</a>;
 });
